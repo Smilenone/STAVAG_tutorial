585527fec00dee6ecf22e6f4204097a693259ace
Smilenone <30334073+Smilenone@users.noreply.github.com>
2025-11-13 03:38:28 +0800

    Update __init__.py

--- STAVAG/__init__.py
+++ STAVAG/__init__.py
@@ -1,5 +1,22 @@
-from .STAVAG import *
+from .STAVAG import (
+    generate_coord_dict,
+    calculate_sps,
+    keep_variant_genes,
+    DVG_detection,
+    TVG_detection,
+    gene_modules,
+)
+
+__all__ = [
+    'generate_coord_dict',
+    'calculate_sps',
+    'keep_variant_genes',
+    'DVG_detection',
+    'TVG_detection',
+    'gene_modules',
+]
 
 __version__ = '1.0.0'
 __author__ = "Qunlun Shen"
 __email__ = "knotnet@foxmail.com"
+
